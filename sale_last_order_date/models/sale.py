dd41b76ef61cb1a9a3b4e0f4400173e22904b46a
onurugur <onurugur@github.com>
2024-09-27 10:41:01 +0300

    sale_last_order_date/ computed sale last order date

--- sale_last_order_date/models/sale.py
+++ sale_last_order_date/models/sale.py
@@ -17,9 +17,23 @@ class SaleOrder(models.Model):
 
     sale_last_order_date = fields.Date('Sale Order Date',default='2024-01-01')
 
+    sale_last_order_date_computed = fields.Date('Computed Sale Order Date',compute='_compute_sale_last_order_date_computed')
+
 
     @api.onchange('sale_last_order_date')
     def _onchange_sale_last_order_date(self):
         if self.sale_last_order_date:
             if self.sale_last_order_date > fields.datetime.today().date():
                 raise UserError('Tarih ileri bir tarih olamaz')
+
+
+    def _compute_sale_last_order_date_computed(self):
+        sale_obj = self.env['sale.order']
+        for sale in self:
+            partner_id = sale.partner_id
+            sale_orders = sale_obj.search([('state','in',['done','sale']),('partner_id','=',partner_id.id),('date_order','<',sale.date_order)])
+            if sale_orders:
+                sorted_sale_orders = sale_orders.sorted('date_order',reverse=True)
+                sale.sale_last_order_date_computed = sorted_sale_orders[0].date_order
+            else:
+                sale.sale_last_order_date_computed = False
